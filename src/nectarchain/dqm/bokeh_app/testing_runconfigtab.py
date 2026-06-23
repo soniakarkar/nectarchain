456b128eb5d0193e0607579265e24f75c8b0ba23
Jean-Philippe Lenain <jlenain@in2p3.fr>
2026-06-23 11:06:29 +0200

    sizing strech both directions

--- src/nectarchain/dqm/bokeh_app/testing_runconfigtab.py
+++ src/nectarchain/dqm/bokeh_app/testing_runconfigtab.py
@@ -5,5 +5,5 @@ from mongodb_explorer import MongoExplorer
 explorer1 = MongoExplorer("mongodb://192.168.30.104:27017", "test", "runconfig")
 explorer2 = MongoExplorer("mongodb://192.168.30.104:27017", "test", "runconfig")
 
-tabs = Tabs(tabs=[explorer1.panel, explorer2.panel])
+tabs = Tabs(tabs=[explorer1.panel, explorer2.panel], sizing_mode="stretch_both")
 curdoc().add_root(tabs)
