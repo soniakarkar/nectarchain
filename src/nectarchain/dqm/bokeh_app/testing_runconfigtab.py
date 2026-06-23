9c753cbef87db48a0bb1bf4ee9e138bf80081d33
Jean-Philippe Lenain <jlenain@in2p3.fr>
2026-06-23 10:31:39 +0200

    relative pathb ilport works but seems strange?

--- src/nectarchain/dqm/bokeh_app/testing_runconfigtab.py
+++ src/nectarchain/dqm/bokeh_app/testing_runconfigtab.py
@@ -1,6 +1,6 @@
 from bokeh.io import curdoc
 from bokeh.models import Tabs
-from nectarchain.dqm.bokeh_app.mongodb_explorer import MongoExplorer
+from mongodb_explorer import MongoExplorer
 
 explorer1 = MongoExplorer("mongodb://192.168.30.104:27017", "test", "runconfig")
 explorer2 = MongoExplorer("mongodb://192.168.30.104:27017", "test", "runconfig")
